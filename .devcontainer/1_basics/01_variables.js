ef14b8c2d94a757981b289dd98bfbd4c06fb61c2
saikatquila3-hub <saikatquila3@gmail.com>
2026-09-17 07:51:32 +0000

    variables

---
+++ .devcontainer/1_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId=14453
+let accountEmail="saikatquila@gamil.com";
+var accoutPassword="4567";
+let AccountCity="kolkata";
+
+
+let AAccountId=20
+
+console.log(AAccountId);
+
+/*
+     pefer not to use var.
+     beacuse of the issue of block scope and funtional scope.
+
+*/
+
+
+accountEmail="saikat@12345"
+
+console.log(accountEmail,accountId,AccountCity,accoutPassword)
+
