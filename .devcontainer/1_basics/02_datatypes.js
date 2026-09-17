b54aa2a284663fac9ae50206c82cec899dbfe469
saikatquila3-hub <saikatquila3@gmail.com>
2026-09-17 07:49:19 +0000

    datatypes

---
+++ .devcontainer/1_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"//treat all js code as a new version
+
+let name='saikat'
+let age=20
+let isLoggedIn=false
+
+
+
+//big int
+//string=> ""
+//boolean=> true/false
+//null=> standalone value
+//undifiend=> value is not assign
+//symbol=> unique
+
+
+//object
+
+console.log(typeof "laddu");
