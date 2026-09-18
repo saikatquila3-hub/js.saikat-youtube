85df5b302b49ba731bf6df5e763552599cc00069
saikatquila3-hub <saikatquila3@gmail.com>
2026-09-18 10:08:06 +0000

    datatypes summary

---
+++ .devcontainer/1_basics/Datatypes-Summary.js
@@ -0,0 +1,41 @@
+//primitive datatypes
+
+
+//7 types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+//  const score=100
+//  const scoreValue=100.3
+
+//  const IsLoggedIn=true;
+// const outsideTemp=null;
+// let accountEmail
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id==anotherId)
+
+
+//Reference Datatypes or Non-Primitives Datatypes.
+
+//Array,Object,Functions.
+
+const heros=["laddu","saktiman","chotabheem"];
+console.log(heros);
+
+//objects
+
+ let myObj={
+     name:"laddu",
+     age:20,
+ }
+
+ const myFunction=function(){
+    console.log("HELLO JAVASCRIPT");
+ }
+
+ console.log(typeof (anotherId));
+
+ console.log(typeof(myFunction));
+ console.log(typeof(myObj));
+ 
+ 
+ 
