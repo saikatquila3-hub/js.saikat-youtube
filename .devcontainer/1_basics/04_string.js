d6350184185332a246e34f14653440887491ecad
saikatquila3-hub <saikatquila3@gmail.com>
2026-09-18 10:44:07 +0000

    String

---
+++ .devcontainer/1_basics/04_string.js
@@ -0,0 +1,21 @@
+let name="saikat"
+let repoCount=50;
+
+
+console.log(name+repoCount+"value");
+//use this
+console.log(`Hello my name is ${name}and my repoCount is${repoCount}`);
+
+ const gameName=new String('laddu')
+// console.log(gameName[0]);
+// console.log(gameName[1]);
+// console.log(gameName[2]);
+
+console.log(gameName.toUpperCase());
+
+const newString=gameName.substring(0,6)
+console.log(newString);
+
+const NewString=gameName.slice(-1,6)
+
+const TewString=newString("hello");
