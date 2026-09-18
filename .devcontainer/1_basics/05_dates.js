478f79bd105b30d1046c6873acbc3e35d1b786f9
saikatquila3-hub <saikatquila3@gmail.com>
2026-09-18 11:23:18 +0000

    Dates

---
+++ .devcontainer/1_basics/05_dates.js
@@ -0,0 +1,21 @@
+//Dates
+
+let myDate=new Date()
+console.log(myDate.toString())
+
+console.log(myDate.toDateString())
+
+console.log(myDate.toLocaleString())
+ let Mydate=new Date(2026,0,26,19)
+ console.log(Mydate.toLocaleDateString());
+
+ //TimeStamp
+
+let myTimeStamp=Date.now()
+console.log(myTimeStamp);
+
+let newDate=new Date()
+console.log(newDate.getMonth())
+console.log(newDate.getTimezoneOffset());
+
+ 
